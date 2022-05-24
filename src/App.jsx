e78a38adb165d9e9be4b1ec7d8bc5a5fd9d318c2
Youssef Bouhachem <ybouhachem@gmail.com>
2022-05-24 17:53:24 +0000

    chore: test preview environment

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Solidy! adsdas
+          Hello world, is there a preview environment?
         </a>
       </header>
     </div>
